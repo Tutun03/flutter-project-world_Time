db5c34d198c660df51fd3a0add0c0a9ffa09e917
Aniket Acharya <aniketacharya30@gmail.com>
2024-12-21 01:00:58 +0530

    widget life cycle

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -8,8 +8,18 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  int counter=0;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();//parent call
+     print('initState function ran');
+  }//first this function will executed when screen loads and it will fire only once.
+
+
   @override
   Widget build(BuildContext context) {
+    print('build function ran');//when build the widget tree then it runs
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
@@ -18,7 +28,11 @@ class _ChooseLocationState extends State<ChooseLocation> {
         centerTitle: true,
         elevation: 0,
       ),
-      body: Text('choose location screen'),//don't need to do safearea here as appbar will be added and entire content will be shifted down due to this later on.
+      body: TextButton(onPressed: () {
+        setState(() {
+          counter=counter+1;
+        });
+      }, child: Text('counter is $counter') ),//don't need to do safearea here as appbar will be added and entire content will be shifted down due to this later on.
     );
   }
 }
